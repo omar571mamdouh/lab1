137590991fffe0d684118de793dae6be213fd687
omar571mamdouh <ok1119076@gmail.com>
2025-02-14 22:13:45 +0200

    lab one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent} from './projects/projects.component';
+import { FooterComponent} from './footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,AboutComponent,SkillsComponent,ProjectsComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
